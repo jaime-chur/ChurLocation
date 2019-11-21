d45ecfb21ea97ab647aa820b55298fe247dbd721
Jaime Jazareno III <jaime@churinc.com>
2019-11-21 08:17:28 +0800

    Initial Commit

---
+++ ChurLocation/ChurLocation.h
@@ -0,0 +1,19 @@
+//
+//  ChurLocation.h
+//  ChurLocation
+//
+//  Created by Jaime Jazareno III on 11/21/19.
+//  Copyright © 2019 Chur Networks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChurLocation.
+FOUNDATION_EXPORT double ChurLocationVersionNumber;
+
+//! Project version string for ChurLocation.
+FOUNDATION_EXPORT const unsigned char ChurLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChurLocation/PublicHeader.h>
+
+
