505eeeadcbc463ef446e650de74c373b4461f06f
Jaime Jazareno III <jaime@churinc.com>
2019-11-21 09:37:40 +0800

    Change target of test

---
+++ ChurLocation/ChurLocation.swift
@@ -0,0 +1,10 @@
+//
+//  ChurLocation.swift
+//  ChurLocation
+//
+//  Created by Jaime Jazareno III on 11/21/19.
+//  Copyright © 2019 Chur Networks. All rights reserved.
+//
+
+@objc public final class ChurLocation: NSObject {
+}
